b9670af74efa4662ca645470c688fe3cbf65feca
Cyber and Programming <aftisaissa37@gmail.com>
2025-07-27 15:30:59 +0200

    Add files via upload

---
+++ box-2D/script.js
@@ -0,0 +1,16 @@
+const canvas = window.document.getElementById('myCanvas');
+canvas.width = 250;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100 , 100 , 30 , 50);
+car.draw(ctx);
+
+animate();
+
+
+function animate() {
+    car.update();
+    canvas.height = window.innerHeight;
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
